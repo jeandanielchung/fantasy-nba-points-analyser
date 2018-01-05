25326afd4eaf3087ff7b6ec9d22f6ec98e0a62c4
Daniel Chung <danielchung@lawn-128-61-18-225.lawn.gatech.edu>
2018-01-05 16:55:16 -0500

    organised code into functions, did basic input checking for url input

--- ESPNScraping.py
+++ ESPNScraping.py
@@ -38,64 +38,65 @@ def filter_by_min(games, operator, value):
 	else:
 		return filter(lambda x: x.min > value, games)
 
-def main():
-	# player_page_url = raw_input("enter link to player's game log: ")
-	player_page_url = raw_input("enter link to player's game log: ") if len(sys.argv) < 2 else sys.argv[1]
-	page = urllib2.urlopen(player_page_url)
+def scrape(page):
+	try:
+		soup = BeautifulSoup(page, 'html.parser')
 
-	soup = BeautifulSoup(page, 'html.parser')
+		stat_table = soup.find("div", class_="mod-player-stats")
 
-	stat_table = soup.find("div", class_="mod-player-stats")
+		regular_stats = stat_table.find(lambda x: x.name == "tr" and "stathead" in x['class'] and "REGULAR" in x.string.upper()) # TODO: verify format of regular
 
-	regular_stats = stat_table.find(lambda x: x.name == "tr" and "stathead" in x['class'] and "REGULAR" in x.string.upper()) # TODO: verify format of regular
+		games = []
 
-	games = []
-
-	for cur_row in regular_stats.next_siblings:
-		if 'stathead' in cur_row['class'] or cur_row.contents[0].string in ['Averages', 'Totals']: # stop before preseason or Averages/Totals row
-			break
-		elif ('evenrow' in cur_row['class'] or 'oddrow' in cur_row['class']) and '/' in cur_row.contents[0].string: # make sure that first value is a date
-			games.append(cur_row)
+		for cur_row in regular_stats.next_siblings:
+			if 'stathead' in cur_row['class'] or cur_row.contents[0].string in ['Averages', 'Totals']: # stop before preseason or Averages/Totals row
+				break
+			elif ('evenrow' in cur_row['class'] or 'oddrow' in cur_row['class']) and '/' in cur_row.contents[0].string: # make sure that first value is a date
+				games.append(cur_row)
 
 
-	# games = regular_stats.find_all("tr", class_=["evenrow", "oddrow"]) # going to need to 
+		# games = regular_stats.find_all("tr", class_=["evenrow", "oddrow"]) # going to need to 
 
-	# print(games)
+		# print(games)
 
 
-	# gets the column names to see what the stats correspond with
-	col_head = soup.find(class_='colhead')
-	heads = [head.string if head.string != '3PM-3PA' else '3PM' for head in col_head.find_all("td")]
+		# gets the column names to see what the stats correspond with
+		col_head = soup.find(class_='colhead')
+		heads = [head.string if head.string != '3PM-3PA' else '3PM' for head in col_head.find_all("td")]
 
-	wanted_stats = ['DATE', 'MIN', '3PM', 'REB', 'AST', 'BLK', 'STL', 'TO', 'PTS'] # can generalise this
-	scoring = {'date': 0, 'min': 0, 'threes': 1, 'reb': 2, 'ast': 2, 
-	'blk': 4, 'stl': 3, 'to': -1, 'pts': 1, 'dd': 5, 'td': 10}
+		wanted_stats = ['DATE', 'MIN', '3PM', 'REB', 'AST', 'BLK', 'STL', 'TO', 'PTS'] # can generalise this
+		scoring = {'date': 0, 'min': 0, 'threes': 1, 'reb': 2, 'ast': 2, 
+		'blk': 4, 'stl': 3, 'to': -1, 'pts': 1, 'dd': 5, 'td': 10}
 
-	game_objs = []
+		game_objs = []
 
-	for game in games:
-		stats = game.find_all('td')
+		for game in games:
+			stats = game.find_all('td')
 
-		if stats[0].string == 'Averages':
-			break
+			if stats[0].string == 'Averages':
+				break
 
-		statVals = []
+			statVals = []
 
-		for i, stat in enumerate(stats):
-			if (heads[i] in wanted_stats):
-				statStr = stat.string if stat.string else 'N/A'
-				if heads[i] == '3PM': statStr = statStr.split('-')[0]
-				if heads[i] == 'DATE': statStr = statStr.split(' ')[1]
-				# TODO: change the 3pm stat
-				statVals.append(statStr)
+			for i, stat in enumerate(stats):
+				if (heads[i] in wanted_stats):
+					statStr = stat.string if stat.string else 'N/A'
+					if heads[i] == '3PM': statStr = statStr.split('-')[0]
+					if heads[i] == 'DATE': statStr = statStr.split(' ')[1]
+					# TODO: change the 3pm stat
+					statVals.append(statStr)
 
-		game_obj = Game(statVals)
-		print(str(game_obj) + ' || ' + '{:>3}'.format(str(game_obj.get_fpts(scoring))))
-		game_objs.append(game_obj)
+			game_obj = Game(statVals)
+			print(str(game_obj) + ' || ' + '{:>3}'.format(str(game_obj.get_fpts(scoring))))
+			game_objs.append(game_obj)
 
-	print('Average: ' + str(find_ave(game_objs, scoring)))
-	print()
+		print('Average: ' + str(find_ave(game_objs, scoring)))
+		print()
+		user_prompt(game_objs, scoring)
+	except:
+		print("there was an error reading the given url, make sure that the page is a valid ESPN NBA player's gamelog e.g. (http://www.espn.com/nba/player/gamelog/_/id/3908845/john-collins)")
 
+def user_prompt(game_objs, scoring):
 	while (True):
 		query = raw_input('Enter filter type (date, min etc.) or q to quit: ').strip()
 		if (query == 'q'):
@@ -131,6 +132,14 @@ def main():
 
 
 	print('bye!')
+
+def main():
+	player_page_url = raw_input("enter link to player's game log: ") if len(sys.argv) < 2 else sys.argv[1]
+	try:
+		page = urllib2.urlopen(player_page_url)
+		scrape(page)
+	except:
+		print("given url is not valid")
 
 if __name__ == "__main__":
 	main()
